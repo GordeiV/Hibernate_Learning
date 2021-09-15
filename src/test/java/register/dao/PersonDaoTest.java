d614c19772200ca085adc2a8d71de90d7ea66311
Gordei <gordei_vihlyaev@mail.ru>
2021-09-15 18:57:28 +0500

    Dao completed

--- src/test/java/register/dao/PersonDaoTest.java
+++ src/test/java/register/dao/PersonDaoTest.java
@@ -3,6 +3,8 @@ package register.dao;
 import org.junit.Assert;
 import org.junit.Test;
 import register.domain.Person;
+import register.domain.PersonFemale;
+import register.domain.PersonMale;
 
 import java.util.List;
 
@@ -14,9 +16,18 @@ public class PersonDaoTest {
         PersonDao personDao = new PersonDao();
         List<Person> persons = personDao.findPersons();
         persons.forEach(p -> {
-            System.out.println(p.getFirstName());
-            System.out.println(p.getClass().getSimpleName());
-            System.out.println(p.getPassports().size());
+            System.out.println("Name: " + p.getFirstName());
+            System.out.println("Class for sex: " + p.getClass().getSimpleName());
+            System.out.println("Passports: " + p.getPassports().size());
+            System.out.println("Birth: " + p.getBirthCertificate());
+            if(p instanceof PersonMale) {
+                System.out.println("Birth Certificate: " + ((PersonMale)p).getBirthCertificates().size());
+                System.out.println("Marriage Certificate: " + ((PersonMale)p).getMarriageCertificates().size());
+            } else {
+                System.out.println("Birth Certificate: " + ((PersonFemale)p).getBirthCertificates().size());
+                System.out.println("Marriage Certificate: " + ((PersonFemale)p).getMarriageCertificates().size());
+            }
+            System.out.println();
         });
     }
 }
